5df02d8c70755cf44469bd080952e291494c1f90
honma <136694820+honma-ambient@users.noreply.github.com>
2023-07-19 10:12:43 +0900

    update Testinfra

--- .devcontainer/features/root/test.py
+++ .devcontainer/features/root/test.py
@@ -1,3 +1,15 @@
 def test_awscli_is_installed(host):
   cmd = host.run("aws --version")
   assert cmd.stdout.startswith("aws-cli")
+
+def test_githubcli_is_installed(host):
+  cmd = host.run("gh --version")
+  assert cmd.stdout.startswith("gh")
+
+def test_jqlikes_is_installed(host):
+  cmd = host.run("jq --version")
+  assert cmd.stdout.startswith("jq")
+
+def test_python_is_installed(host):
+  cmd = host.run("python --version")
+  assert cmd.stdout.startswith("Python")
